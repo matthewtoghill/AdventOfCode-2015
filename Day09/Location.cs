0a86cd322bf5a5e940fe40c0530a360d5ed5bf1a
matthewtoghill <matthew.toghill@outlook.com>
2021-05-23 14:43:13 +0100

    Day 9 completed

---
+++ Day09/Location.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Day09
+{
+    public class Location
+    {
+        public string Name { get; set; }
+        public Dictionary<string, int> DestinationDistances { get; set; } = new Dictionary<string, int>();
+
+        public Location() { }
+        public Location(string name) => Name = name;
+
+        public void AddDestination(string destination, int distance)
+        {
+            DestinationDistances.Add(destination, distance);
+        }
+
+        public int GetDistanceTo(string destination)
+        {
+            DestinationDistances.TryGetValue(destination, out int distance);
+            return distance;
+        }
+    }
+}
---
+++ Day09/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day09
+{
+    class Program
+    {
+        private static readonly string[] input = File.ReadAllLines(@"..\..\..\data\day09.txt");
+        static void Main()
+        {
+            BothParts();
+        }
+
+        private static void BothParts()
+        {
+            List<Location> locations = GetLocations(input);
+
+            List<List<Location>> locationPermutations = GetPermutations(locations, locations.Count)
+                .Select(l => l.ToList())
+                .ToList();
+
+            int minDistance = int.MaxValue;
+            int maxDistance = 0;
+
+            foreach (var permutation in locationPermutations)
+            {
+                int distance = 0;
+                for (int i = 0; i < permutation.Count() - 1; i++)
+                    distance += permutation[i].GetDistanceTo(permutation[i + 1].Name);
+
+                minDistance = Math.Min(minDistance, distance);
+                maxDistance = Math.Max(maxDistance, distance);
+            }
+
+            Console.WriteLine($"Part 1: Min Distance: {minDistance}");
+            Console.WriteLine($"Part 2: Max Distance: {maxDistance}");
+        }
+
+        private static List<Location> GetLocations(string[] locationsData)
+        {
+            List<Location> locations = new List<Location>();
+            foreach (var line in locationsData)
+            {
+                string[] info = line.Split(new[] { "to", "=", " " }, StringSplitOptions.RemoveEmptyEntries);
+                string from = info[0];
+                string to = info[1];
+                int distance = int.Parse(info[2]);
+
+                Location fromLoc = locations.Find(l => l.Name == from);
+                if (fromLoc is null)
+                {
+                    Location newFromLocation = new Location(from);
+                    newFromLocation.AddDestination(to, distance);
+                    locations.Add(newFromLocation);
+                }
+                else
+                {
+                    fromLoc.AddDestination(to, distance);
+                }
+
+                Location toLoc = locations.Find(l => l.Name == to);
+                if (toLoc is null)
+                {
+                    Location newToLocation = new Location(to);
+                    newToLocation.AddDestination(from, distance);
+                    locations.Add(newToLocation);
+                }
+                else
+                {
+                    toLoc.AddDestination(from, distance);
+                }
+            }
+            return locations;
+        }
+
+        public static IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length)
+        {
+            if (length == 1) return list.Select(t => new T[] { t });
+
+            return GetPermutations(list, length - 1)
+                .SelectMany(t => list.Where(e => !t.Contains(e)),
+                    (t1, t2) => t1.Concat(new T[] { t2 }));
+        }
+    }
+}
