8f59b221e0352e935fd5f0f38023ba33cd81ca2c
matthewtoghill <matthew.toghill@outlook.com>
2021-06-23 08:10:42 +0100

    Day 14 completed

---
+++ Day14/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day14
+{
+    class Program
+    {
+        private static readonly string[] input = File.ReadAllLines(@"..\..\..\data\day14.txt");
+        static void Main()
+        {
+            int totalRunSeconds = 2503;
+
+            // Create the list of reindeer from the input data
+            List<Reindeer> reindeer = GetReindeer(input);
+
+            // Run for the required number of seconds
+            for (int i = 0; i < totalRunSeconds; i++)
+            {
+                // Tick 1 second for each reindeer
+                reindeer.ForEach(r => r.Tick());
+
+                // Find the current max distance travelled
+                int max = reindeer.Max(r => r.DistanceTravelled);
+
+                // Add a point to each reindeer which has travelled the max distance
+                foreach (var r in reindeer)
+                    if (r.DistanceTravelled == max)
+                        r.Points++;
+            }
+
+            Console.WriteLine($"Part 1: Furthest: {reindeer.Max(r => r.DistanceTravelled)}");
+            Console.WriteLine($"Part 2: Most Points: {reindeer.Max(r => r.Points)}");
+        }
+
+        private static List<Reindeer> GetReindeer(string[] reindeerData)
+        {
+            List<Reindeer> reindeer = new List<Reindeer>();
+            foreach (var line in reindeerData)
+            {
+                string[] info = line.Split();
+                string name = info[0];
+                int speed = int.Parse(info[3]);
+                int duration = int.Parse(info[6]);
+                int rest = int.Parse(info[13]);
+
+                reindeer.Add(new Reindeer(name, speed, duration, rest));
+            }
+
+            return reindeer;
+        }
+    }
+}
---
+++ Day14/Reindeer.cs
@@ -0,0 +1,46 @@
+﻿namespace Day14
+{
+    public class Reindeer
+    {
+
+        public string Name { get; private set; }
+        public int Speed { get; private set; }
+        public int FlyDuration { get; private set; }
+        public int FlyingSecondsRemaining { get; private set; }
+        public int DistanceTravelled { get; private set; }
+        public int RestDuration { get; private set; }
+        public int RestingSecondsRemaining { get; private set; }
+        public bool IsResting => RestingSecondsRemaining > 0;
+        public bool IsFlying => FlyingSecondsRemaining > 0;
+        public int Points { get; set; }
+
+        public Reindeer(string name, int speed, int flyDuration, int restDuration)
+        {
+            Name = name;
+            Speed = speed;
+            FlyDuration = flyDuration;
+            RestDuration = restDuration;
+            FlyingSecondsRemaining = flyDuration;
+        }
+
+        public void Tick()
+        {
+            if (IsResting)
+            {
+                RestingSecondsRemaining--;
+
+                if (RestingSecondsRemaining == 0) FlyingSecondsRemaining = FlyDuration;
+
+                return;
+            }
+
+            if (IsFlying)
+            {
+                FlyingSecondsRemaining--;
+                DistanceTravelled += Speed;
+
+                if (FlyingSecondsRemaining == 0) RestingSecondsRemaining = RestDuration;
+            }
+        }
+    }
+}
