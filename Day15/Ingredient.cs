854f8272cc2f3f702299ef4ba1c3ac2a2fa81b0c
matthewtoghill <matthew.toghill@outlook.com>
2021-09-09 12:23:42 +0100

    Update Day 15

--- Day15/Ingredient.cs
+++ Day15/Ingredient.cs
@@ -5,7 +5,7 @@
         public string Name { get; set; }
         public int Capacity { get; set; }
         public int Durability { get; set; }
-        public int Flavor { get; set; }
+        public int Flavour { get; set; }
         public int Texture { get; set; }
         public int Calories { get; set; }
 
@@ -17,7 +17,7 @@
             Name = vals[0];
             Capacity = int.Parse(vals[2]);
             Durability = int.Parse(vals[4]);
-            Flavor = int.Parse(vals[6]);
+            Flavour = int.Parse(vals[6]);
             Texture = int.Parse(vals[8]);
             Calories = int.Parse(vals[10]);
         }
--- Day15/Program.cs
+++ Day15/Program.cs
@@ -1,39 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace Day15
 {
     class Program
     {
-        private static string[] input = new[]
-        {
-           "Sprinkles: capacity 5, durability -1, flavor 0, texture 0, calories 5",
-           "PeanutButter: capacity -1, durability 3, flavor 0, texture 0, calories 1",
-           "Frosting: capacity 0, durability -1, flavor 4, texture 0, calories 6",
-           "Sugar: capacity -1, durability 0, flavor 0, texture 2, calories 8"
-        };
-
+        private static readonly string[] input = File.ReadAllLines(@"..\..\..\data\day15.txt");
         static void Main()
         {
+            // Create list of Ingredients from input data
             List<Ingredient> ingredients = input.Select(t => new Ingredient(t)).ToList();
 
             long maxScore = 0, maxScoreCals = 0;
 
+            // Use nested for loops to calculate all quantity permutations of each ingredient
+            // Each permutation must total 100 teaspoons of ingredients
+            // x represents the quantity of the 1st Ingredient: Sprinkles
             for (int x = 0; x <= 100; x++)
             {
+                // y represents the quantity of the 2nd Ingredient: PeanutButter
                 for (int y = 0; y <= 100 - x; y++)
                 {
+                    // z represents the quantity of the 3rd Ingredient: Frosting
+                    // the remaining ingredient Sugar is calculated as (100 - x - y - z)
                     for (int z = 0; z <= 100 - y - x; z++)
                     {
-                        long c = ingredients[0].Capacity * x + ingredients[1].Capacity * y + ingredients[2].Capacity * z + ingredients[3].Capacity * (100 - x - y - z);
-                        long d = ingredients[0].Durability * x + ingredients[1].Durability * y + ingredients[2].Durability * z + ingredients[3].Durability * (100 - x - y - z);
-                        long f = ingredients[0].Flavor * x + ingredients[1].Flavor * y + ingredients[2].Flavor * z + ingredients[3].Flavor * (100 - x - y - z);
-                        long t = ingredients[0].Texture * x + ingredients[1].Texture * y + ingredients[2].Texture * z + ingredients[3].Texture * (100 - x - y - z);
+                        int sugarQuantity = 100 - x - y - z;
 
+                        // Calculate scores for each property: Capacity, Durability, Flavour, and Texture
+                        // by multiplying each Ingredients respective property by the quantity for the current permutation
+                        long c = ingredients[0].Capacity * x + ingredients[1].Capacity * y + ingredients[2].Capacity * z + ingredients[3].Capacity * sugarQuantity;
+                        long d = ingredients[0].Durability * x + ingredients[1].Durability * y + ingredients[2].Durability * z + ingredients[3].Durability * sugarQuantity;
+                        long f = ingredients[0].Flavour * x + ingredients[1].Flavour * y + ingredients[2].Flavour * z + ingredients[3].Flavour * sugarQuantity;
+                        long t = ingredients[0].Texture * x + ingredients[1].Texture * y + ingredients[2].Texture * z + ingredients[3].Texture * sugarQuantity;
+
+                        // Calculate the total score for this permutation by multiplying each score together
+                        // if any score is less than 0 then the total score is 0
                         long score = c < 0 || d < 0 || f < 0 || t < 0 ? 0 : c * d * f * t;
 
+                        // Store the Max Score found across all permutations
                         maxScore = Math.Max(score, maxScore);
+
+                        // Store the Max Calories Score across all permutations that have a Calorie score of 500 
                         if (score > maxScoreCals)
                         {
                             long calories = ingredients[0].Calories * x + ingredients[1].Calories * y + ingredients[2].Calories * z + ingredients[3].Calories * (100 - x - y - z);
